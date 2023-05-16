a97e21c02ae23fa95bbd19bc1197199dcdc41e5c
MUsharafKh <108793645+MUsharafKh@users.noreply.github.com>
2023-05-16 10:20:40 +0530

    MixMasterConect

---
+++ MixMasterConnect/resources/views/index.blade.php
@@ -0,0 +1,71 @@
+@extends('layouts.master')
+@section('content')
+<div role="main" class="main">
+				<div class="slider-container rev_slider_wrapper slider-container-full-height">
+					<div id="revolutionSlider" class="slider rev_slider" data-version="5.4.8" data-plugin-revolution-slider data-plugin-options="{'delay': 9000, 'sliderLayout': 'fullscreen', 'gridwidth': [1140,960,720,540], 'gridheight': [900,900,900,900], 'disableProgressBar': 'off', 'responsiveLevels': [4096,1200,992,576], 'navigation' : {'arrows': { 'enable': false, 'hide_under': 767, 'style': 'slider-arrows-style-2' }, 'bullets': {'enable': false, 'style': 'bullets-style-1', 'h_align': 'center', 'v_align': 'bottom', 'space': 7, 'v_offset': 35, 'h_offset': 0}}}">
+						<ul>
+							<li class="slide-overlay slide-overlay-level-9" data-transition="fade">
+								<img src="assets/img/slides/multi-purpose/home3.jpg"  
+									alt=""
+									data-bgposition="center center" 
+									data-bgfit="cover" 
+									data-bgrepeat="no-repeat" 
+									class="rev-slidebg">
+
+								<div class="tp-caption bg-primary"
+									data-x="['120','23','53','53']"
+									data-y="['371','371','387','397']"
+									data-start="1500"
+									data-width="['20','20','17','10']"
+									data-height="['157','157','102','83']"
+									data-transform_in="x:[-100%];opacity:0;s:500;"
+									data-transform_idle="skX:19deg"
+									data-transform_out="x:[-100%];opacity:0;s:500;"></div>
+
+				
+
+								<div class="tp-caption text-color-light font-weight-bold font-primary effect-text-background"
+									data-x="center"
+									data-y="center" data-voffset="-30"
+									data-start="1000"
+									data-fontsize="['125','125','80','57']"
+									data-lineheight="['130','130','90','62']"
+									data-transform_in="y:[-100%];opacity:0;s:500;"
+									data-transform_out="opacity:0;s:500;">Discover the Perfect Beats</div>
+
+								<div class="tp-caption text-color-light font-primary"
+									data-x="center" data-hoffset="['-150','-150','-125','-50']"
+									data-y="center" data-voffset="['58','58','25','18']"
+									data-start="1900"
+									data-fontsize="['38','38','24','24']"
+									data-transform_in="y:[100%];opacity:0;s:500;"
+									data-transform_out="y:[100%];opacity:0;s:500;">World-Class Technology</div>
+
+								
+                        
+							</li>
+
+                            <li class="slide-overlay slide-overlay-level-9" data-transition="fade">
+                            <a class="tp-caption btn btn-rounded btn-primary font-weight-semibold"
+									href="#ourwork"
+									data-hash
+									data-hash-offset="90"
+									data-x="center" data-hoffset="['305','305','175','0']"
+									data-y="center" data-voffset="['65','65','35','150']"
+									data-start="2200"
+									data-whitespace="nowrap"	
+									data-fontsize="['13','13','13','13']"
+									data-paddingtop="['13','13','10','10']"
+									data-paddingbottom="['13','13','10','10']"
+									data-paddingleft="['65','65','50','50']"
+									data-paddingright="['65','65','50','50']"						 
+									data-transform_in="y:[100%];s:500;"
+									data-transform_out="y:[100%];opacity:0;s:500;"
+									data-mask_in="x:0px;y:0px;">Logging</a>
+                            </li>
+							
+						</ul>
+					</div>
+				</div>
+
+@endsection
---
+++ MixMasterConnect/resources/views/layouts/Includes/footer.blade.php
@@ -0,0 +1,77 @@
+
+
+
+<footer id="footer" class="footer footer-hover-links-light pb-5 mt-0 parallax-effect" data-plugin-image-background style="background-image: url('assets/img/slides/multi-purpose/home2.jpg'); background-position: center 100%; background-attachment: fixed; background-size: cover;">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-4 mb-4 mb-lg-0">
+                <h2 class="text-1 mb-3">ABOUT MixMasterConnect</h2>
+                <p>Through our innovative platform, DJs can create personalized profiles, share their mixes, and connect with a global audience. Participants have the power to influence the music played at events by making song requests, creating playlists, and engaging with the vibrant DJ community.</p>
+            </div>
+        
+            <div class="col-lg-3">
+                <h2 class="text-1 mb-3">QUICK LINKS</h2>
+                <ul class="list list-unstyled">
+                    <li class="mb-2"><i class="fas fa-angle-right me-2 ms-1"></i> <a href="https://themeforest.net/item/ezy-responsive-multipurpose-html5-template/21814871">playlists</a></li>
+                    <li class="mb-2"><i class="fas fa-angle-right me-2 ms-1"></i> <a href="contact-us-2.html">Events</a></li>
+                    <li class="mb-2"><i class="fas fa-angle-right me-2 ms-1"></i> <a href="contact-us-3.html">DJ Profile</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="footer-copyright">
+        <div class="container container-xl">
+            <div class="row text-center text-md-start align-items-center">
+                <div class="col-md-7 col-lg-8">
+                    <ul class="social-icons social-icons-transparent social-icons-icon-light social-icons-lg">
+                        <li class="social-icons-facebook"><a href="http://www.facebook.com/" target="_blank" title="Facebook"><i class="fab fa-facebook-f"></i></a></li>
+                        <li class="social-icons-twitter"><a href="http://www.twitter.com/" target="_blank" title="Twitter"><i class="fab fa-twitter"></i></a></li>
+                        <li class="social-icons-instagram"><a href="http://www.instagram.com/" target="_blank" title="Instagram"><i class="fab fa-instagram"></i></a></li>
+                    </ul>
+                </div>
+                <div class="col-md-5 col-lg-4">
+                    <p class="text-md-end pb-0 mb-0">© 2023 MixMasterConnect</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</footer>
+
+<!-- Vendor -->
+<script src="assets/vendor/jquery/jquery.min.js"></script>
+<script src="assets/vendor/jquery.appear/jquery.appear.min.js"></script>
+<script src="assets/vendor/jquery.easing/jquery.easing.min.js"></script>
+<script src="assets/vendor/jquery.cookie/jquery.cookie.js"></script>
+<script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="assets/vendor/common/common.min.js"></script>
+<script src="assets/vendor/jquery.validation/jquery.validate.min.js"></script>
+<script src="assets/vendor/jquery.easy-pie-chart/jquery.easypiechart.min.js"></script>
+<script src="assets/vendor/jquery.gmap/jquery.gmap.min.js"></script>
+<script src="assets/vendor/jquery.lazyload/jquery.lazyload.min.js"></script>
+<script src="assets/vendor/isotope/jquery.isotope.min.js"></script>
+<script src="assets/vendor/owl.carousel/owl.carousel.min.js"></script>
+<script src="assets/vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
+<script src="assets/vendor/vide/jquery.vide.min.js"></script>
+<script src="assets/vendor/vivus/vivus.min.js"></script>
+
+<!-- Theme Base, Components and Settings -->
+<script src="assets/js/theme.js"></script>
+
+<!-- Current Page Vendor and Views -->
+<script src="assets/vendor/rs-plugin/js/jquery.themepunch.tools.min.js"></script>
+<script src="assets/vendor/rs-plugin/js/jquery.themepunch.revolution.min.js"></script>
+
+<!-- Theme Custom -->
+<script src="assets/js/custom.js"></script>
+
+<!-- Theme Initialization Files -->
+<script async src="assets/js/theme.init.js"></script>
+
+<!-- Examples -->
+<script src="assets/js/examples/examples.lightboxes.js"></script>
+
+
+</body>
+
+</html>
